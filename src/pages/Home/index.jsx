0d44d2d70b23a9d0770f1c79b4f10955d53f1d60
itsmiraz <mirajulislamofficial044@gmail.com>
2024-10-07 07:02:55 +0600

    new change

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -5,20 +5,20 @@ import History from "./components/history";
 import Missions from "./components/missions";
 import CTA from "./components/cta";
 import BeyondWater from "./components/beyondWater";
-import Occasion from "./components/Occasion";
+import Occasion from "./components/occasion";
 import Awards from "./components/awards";
 const Home = () => {
   return (
     <div>
-     <Hero />
-       <EcoSolutions />
-    <BeyondWater />
-       <Missions />
-       <History />
+      <Hero />
+      <EcoSolutions />
+      <BeyondWater />
+      <Missions />
+      <History />
       <Occasion />
-    <Awards/>
-       <Clients />
- <CTA /> 
+      <Awards />
+      <Clients />
+      <CTA />
     </div>
   );
 };
