2ad35464d331d7215e3fc234e54164b76cfbfbee
itsmiraz <mirajulislamofficial044@gmail.com>
2024-10-08 08:02:42 +0600

    expertise section added

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -1,68 +1,90 @@
+import restaurant from "../assets/images/projectoccassion/restuarant.png";
+import marathon from "../assets/images/projectoccassion/marathon.png";
+import ski from "../assets/images/projectoccassion/ski-resort.png";
+import festival from "../assets/images/projectoccassion/festival.png";
+import sports from "../assets/images/projectoccassion/sports.png";
 
-import restaurant from '../assets/images/projectoccassion/restuarant.png'
-import marathon from '../assets/images/projectoccassion/marathon.png'
-import ski from '../assets/images/projectoccassion/ski-resort.png'
-import festival from '../assets/images/projectoccassion/festival.png'
-import sports from '../assets/images/projectoccassion/sports.png'
+import award1 from "../assets/images/awards/award-1.png";
+import award2 from "../assets/images/awards/award-2.png";
+import award3 from "../assets/images/awards/award-3.png";
 
-import award1 from '../assets/images/awards/award-1.png'
-import award2 from '../assets/images/awards/award-2.png'
-import award3 from '../assets/images/awards/award-3.png'
-
-
+import OnsiteIcon from "../assets/icons/onSiteLogo.svg";
+import PartnerShipIcon from "../assets/icons/partnerShip.svg";
+import Opportunities from "../assets/icons/oppurtunitites.svg";
 
 export const AwardContent = [
-    {
-        img:award1,
-        title:'Certificate Earned',
-        desc:"We Took Advantage For Every Event That More Need A Ball O shot",
-        code:"X2342435354353"
-    },
-    {
-        img:award2,
-        title:'Certificate Earned',
-        desc:"We Took Advantage For Every Event That More Need A Ball O shot",
-        code:"X2342435354353"
-    },
-    {
-        img:award3,
-        title:'Certificate Earned',
-        desc:"We Took Advantage For Every Event That More Need A Ball O shot",
-        code:"X2342435354353"
-    },
+  {
+    img: award1,
+    title: "Certificate Earned",
+    desc: "We Took Advantage For Every Event That More Need A Ball O shot",
+    code: "X2342435354353",
+  },
+  {
+    img: award2,
+    title: "Certificate Earned",
+    desc: "We Took Advantage For Every Event That More Need A Ball O shot",
+    code: "X2342435354353",
+  },
+  {
+    img: award3,
+    title: "Certificate Earned",
+    desc: "We Took Advantage For Every Event That More Need A Ball O shot",
+    code: "X2342435354353",
+  },
+];
+
+export const OccasionContent = [
+  {
+    img: restaurant,
+    title: "Restauration & Bar",
+    description:
+      "Could be used to replace single-use packets and plating amazing dishes.",
+  },
+  {
+    img: marathon,
+    title: "Marathon & Race",
+    description:
+      "A quick and easy way to refresh and re-energize. As a consequence, it reduces leftover pollution along with the hours of work needed to clean it.",
+  },
+  {
+    img: ski,
+    title: "Ski Resorts",
+    description:
+      "Situated on the top of the mountains, a Ball-O-Shot would be more than a good idea for enjoyers to take a quick shot of their favorite energy drink.",
+  },
+  {
+    img: festival,
+    title: "Festival & Events",
+    description:
+      "The Ball-O-Shot being biodegradable would reduce the amount of waste left on site during these events and the workforce needed to collect them.",
+  },
+  {
+    img: sports,
+    title: "Sports",
+    description:
+      "We aim to transfer our recipe to the field of sports to make it easy to consume fluids during the most important moments.",
+  },
+];
+
+export const AboutExpertise = [
+  {
+    icon:<OnsiteIcon/>,
+    title: "On-Site Presence",
+    description:
+      "Bring the excitement of Ball-O-Shot to your event and impress your guests with a unique beverage experience.",
+  },
+  {
+    icon: <PartnerShipIcon/>,
+    title: "Long Term Partnership",
+    description:
+      "Looking to offer Ball-O-Shot permanently at your venue? We can make that happen.",
+  },
+  {
+    icon: <Opportunities/>,
+    title: "Sponsor Opportunities",
+    description:
+      "Partner with us for sponsorship opportunities that elevate your event and deliver unforgettable experiences.",
+  }
 ]
 
 
-
-export const OccasionContent = [
-    {
-      img: restaurant,
-      title: "Restauration & Bar",
-      description:
-        "Could be used to replace single-use packets and plating amazing dishes."
-    },
-    {
-      img: marathon,
-      title: "Marathon & Race",
-      description:
-        "A quick and easy way to refresh and re-energize. As a consequence, it reduces leftover pollution along with the hours of work needed to clean it."
-    },
-    {
-      img: ski,
-      title: "Ski Resorts",
-      description:
-        "Situated on the top of the mountains, a Ball-O-Shot would be more than a good idea for enjoyers to take a quick shot of their favorite energy drink."
-    },
-    {
-      img: festival,
-      title: "Festival & Events",
-      description:
-        "The Ball-O-Shot being biodegradable would reduce the amount of waste left on site during these events and the workforce needed to collect them."
-    },
-    {
-      img: sports,
-      title: "Sports",
-      description:
-        "We aim to transfer our recipe to the field of sports to make it easy to consume fluids during the most important moments."
-    }
-  ];
--- src/pages/About/components/expertise.jsx
+++ src/pages/About/components/expertise.jsx
@@ -1,8 +1,26 @@
+import { AboutExpertise } from "../../../constants";
 
 const Expertise = () => {
   return (
-    <div>Expertise</div>
-  )
-}
+    <div className="pt-[89px] flex justify-center md:flex-row flex-col gap-x-[31px]">
+      {AboutExpertise.map((item, i) => (
+        <div
+          className="bg-[#F8F8F8] md:size-[400px] rounded-[33px]  flex flex-col justify-center items-center pt-[72px] pb-[34px] px-5"
+          key={i}
+        >
+          <div>{item.icon}</div>
+          <div>
+            <h2 className="text-[#3F3F3F] text-center pt-[46px] text-[28px] leading-[35px] font-bold">
+              {item.title}
+            </h2>
+            <p className="text-[20px] text-[#363636] pt-[12px] leading-[25px] text-center">
+              {item.description}
+            </p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
 
-export default Expertise
+export default Expertise;
